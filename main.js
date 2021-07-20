6cff3e436b71a9ead592d588c44c5172723957e9
LeJoW <elle.git.wilke@gmail.com>
2021-07-20 18:31:49 +0200

    GHPages

--- main.js
+++ main.js
@@ -2,14 +2,7 @@ document.querySelector("button").addEventListener("click", function () {
     navigator.usb
         .requestDevice({ filters: [{ vendorId: 0x05ac }] })
         .then((device) => {
-            for (const key in device) {
-                const element = device[key];
-                if (!(element instanceof Function)) {
-                    document.querySelector(
-                        "#result"
-                    ).innerHTML += `<strong>${key}:</strong> ${element}<br>\n`;
-                }
-            }
+            document.querySelector("result").innerHTML = JSON.stringify(device);
         })
         .catch((error) => {
             console.error(error);
