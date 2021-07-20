e008b179186849b9c72a467de0d46a238b861965
LeJoW <elle.git.wilke@gmail.com>
2021-07-20 19:02:43 +0200

    ----

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 document.querySelector("button").addEventListener("click", function () {
     navigator.usb
-        .requestDevice({ filters: [{ vendorId: 0x05ac }] })
+        .requestDevice({
+            filters: [
+                { vendorId: 0x05ac }, // Apple Inc
+                { vendorId: 0x04a9 }, // Canon Inc
+            ],
+        })
         .then((device) => {
-            document.querySelector("result").innerHTML = JSON.stringify(device);
+            document.querySelector("result").innerHTML = JSON.stringify(
+                device,
+                null,
+                4
+            );
         })
         .catch((error) => {
             console.error(error);
